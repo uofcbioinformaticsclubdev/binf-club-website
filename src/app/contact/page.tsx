2cfaa4f6e136a689a8fae3c337790962e085f259
Kieran Wood <kieran@canadiancoding.ca>
2025-11-18 19:58:18 -0700

    Added svg wave

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -29,9 +29,13 @@ export default function ContactPage() {
       <img src="https://images.unsplash.com/photo-1713947506934-c0b6519e069d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332" alt="" className="object-cover w-full rounded-md xl:col-span-3 dark:bg-gray-500" />
     </div>
   </section>
-  <section className="w-full bg-primary h-[45vh]">
-    
-        <div className="relative w-full h-full">
+
+  <section className="w-full bg-cream h-[45vh]">
+      
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="var(--color-seagreen)" fill-opacity="1" d="M0,288L26.7,261.3C53.3,235,107,181,160,176C213.3,171,267,213,320,240C373.3,267,427,277,480,256C533.3,235,587,181,640,149.3C693.3,117,747,107,800,128C853.3,149,907,203,960,197.3C1013.3,192,1067,128,1120,133.3C1173.3,139,1227,213,1280,234.7C1333.3,256,1387,224,1413,208L1440,192L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path>
+      </svg>
+        <div className="relative w-full h-full mt-[-3px]">
           <span className="absolute top-0 left-0 z-50">
                 <svg
                   width="189"
